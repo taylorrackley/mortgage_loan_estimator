41dfad37fd070d242b44584ed7397bafa78ae4da
Taylor Rackley <taylorrackley@proton.me>
2022-12-22 19:29:15 -0500

    Form formation

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col col="12">
+          <Form />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Form from './components/Form.vue';
+
+@Component({
+  components: {
+    Form,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import App from './App.vue';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import './app.scss';
+
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+new Vue({
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
